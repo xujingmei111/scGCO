a3bae4f8d5e51165528f28265f715a6e92ac2bb8
WangPeng-Lab <45260020+WangPeng-Lab@users.noreply.github.com>
2018-11-22 23:21:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 setup(
         name='scGCO',
-        version='1.1.0',
+        version='0.0.1',
         description='Spatial Gene',
-        url='https://github.com/Wanglab/scGCO',
+        url='https://github.com/WangPeng-Lab/scGCO',
         packages=find_packages(),
         include_package_data=True,
         install_requires=['parmap','numpy','matplotlib','scipy','sklearn','Cython','pygco',
