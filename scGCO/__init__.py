66143c7cd3e21b5f512cfcc359385ce4b5ebfbfc
WangPeng-Lab <fengwanwan@picb.ac.cn>
2018-12-04 23:16:37 +0800

    add scGCO

---
+++ scGCO/__init__.py
@@ -0,0 +1,1 @@
+from .scgco import *
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 setup(
         name='scGCO',
         version='0.0.1',
-        description='Spatial Gene',
+        description='single-cell graph cuts optimization',
         url='https://github.com/WangPeng-Lab/scGCO',
         packages=find_packages(),
         include_package_data=True,
-        install_requires=['parmap','numpy','matplotlib','scipy','sklearn','Cython','pygco',
-                        'tqdm','networkx','shapely'],
+        install_requires=['pandas','numpy','matplotlib >=2.0.0, <3.0.0','scipy','sklearn','parmap','Cython','pygco',
+                        'tqdm','networkx','shapely','statsmodels'],
         author='Peng Wang',
         author_email='wangpeng@picb.ac.cn',
         license='MIT'
